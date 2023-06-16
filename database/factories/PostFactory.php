aecb9edfef6e10fa342a23aa4ea17d0c42675b39
Pankaj Singh <110153178+pankajBiinnovations@users.noreply.github.com>
2023-06-17 01:19:09 +0530

    commited

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as faker;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $faker=Faker::create();
+        return [
+            'name'=>$faker->name()
+        ];
+    }
+}
