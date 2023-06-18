9c41e576e6edd9a71574ba6b41bd5b82cb1d1878
Pankaj Singh <110153178+pankajBiinnovations@users.noreply.github.com>
2023-06-19 00:27:22 +0530

    pushed

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+        $token = JWTAuth::fromUser($user);
+        $user->remember_token=$token;
+        return response()->json(['message' => 'Registration successful','token'=>$token], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function profile(Request $request)
+    {
+        $user = JWTAuth::parseToken()->authenticate();
+
+        // Return user profile data
+        return response()->json([
+            'name' => $user->name,
+            'email' => $user->email,
+           
+            // Add any other desired user profile data
+        ]);
+    }
+}
